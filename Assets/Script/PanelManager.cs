d216101de0a79de6d5ee15f50a784313a6ddac97
SusanaSantosM <ssantosmallqui@danielcastelao.org>
2025-04-27 17:25:29 +0200

    Closes #9

--- Assets/Script/PanelManager.cs
+++ Assets/Script/PanelManager.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PanelManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("OptionPanel")]
+    public Slider volumenFx;
+    public Slider volumeMusic;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    [Header("Panels")]
+    public GameObject MainPanel;
+    public GameObject OptionPanel;
+    public GameObject HelpPanel;
+
+    // Controla los paneles que se verán según su selección
+    public void OpenPanel(GameObject panel) 
+    { 
+        MainPanel.SetActive(false);
+        OptionPanel.SetActive(false);
+        HelpPanel.SetActive(false);
+
+        panel.SetActive(true);
     }
 }
