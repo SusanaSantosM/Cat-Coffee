88ef99f5f1fbb69b407ea6b0f1cc8b52853c0a64
SusanaSantosM <ssantosmallqui@danielcastelao.org>
2025-04-27 19:44:19 +0200

    Agregamos objetos para solucionar el bug

--- Assets/Script/PanelManager.cs
+++ Assets/Script/PanelManager.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class PanelManager : MonoBehaviour
 {
     [Header("OptionPanel")]
-    public Slider volumenFx;
+    public Slider volumeFx;
     public Slider volumeMusic;
 
     [Header("Panels")]
     public GameObject MainPanel;
     public GameObject OptionPanel;
     public GameObject HelpPanel;
+
+    public AudioMixer mixer;
+    public AudioSource fxsource;
+    public AudioClip clickSound;
+
+    
+    private void Awake()
+    {
+        volumeFx.onValueChanged.AddListener(ChangeVolumeFx);
+        volumeMusic.onValueChanged.AddListener(ChangeVolumeMaster);
+    }
+
 
     // Controla los paneles que se verán según su selección
     public void OpenPanel(GameObject panel) 
@@ -20,7 +33,24 @@ public class PanelManager : MonoBehaviour
         HelpPanel.SetActive(false);
 
         panel.SetActive(true);
-        
+        PlaySoundButton();
+    }
+
+    
+    public void ChangeVolumeMaster(float v)
+    {
+        mixer.SetFloat("VolMaster", v);
+    }
+
+    public void ChangeVolumeFx(float v)
+    {
+        mixer.SetFloat("VolFx", v);
+    }
+
+
+    public void PlaySoundButton() 
+    {
+        fxsource.PlayOneShot(clickSound);
     }
 
 }
