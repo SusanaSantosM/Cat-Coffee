d93db87ca6abaf7536fbcd0e8e531d5ddba76634
SusanaSantosM <ssantosmallqui@danielcastelao.org>
2025-06-11 19:35:18 +0200

    agregamos nueva fuente para los botones

--- Assets/Script/LevelManager.cs
+++ Assets/Script/LevelManager.cs
@@ -1,43 +1,14 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-
-    // Implementación del patrón Singleton para LevelManager
-    public static LevelManager Instance { get; private set; }
-
-    // Instancias de tus gestores
-    public GestorPedidos Pedidos { get; private set; }
-    public GestorPuntuacion Puntuacion { get; private set; }
-
-
     public void BotonStart()
     {   
-        // PAra ir a la escena del juego
+        // Para ir a la escena del juego
         SceneManager.LoadScene(1);
     }
-
-
-    void Awake()
-    {
-        // Asegura que solo haya una instancia de LevelManager
-        if (Instance != null && Instance != this)
-        {
-            Destroy(gameObject);
-        }
-        else
-        {
-            Instance = this;
-            // Opcional: para que el gestor persista entre escenas
-            // DontDestroyOnLoad(gameObject);
-
-            // Inicializa tus gestores
-            Pedidos = new GestorPedidos();
-            Puntuacion = new GestorPuntuacion();
-        }
-    }
-
 
     public void BotonEnd()
     {
@@ -51,8 +22,5 @@ public class LevelManager : MonoBehaviour
         Debug.Log("No se puede cerrar en WebGL. Por favor, cierra la pestaña manualmente.");
         // Redirigir a una URL externa (ej: tu web personal)
     }
-
-
-
 
 }
